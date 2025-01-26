4fe64aecfaa55f07bd76a204122ef1f2c4a5824c
GokceSoylu <gokcesoylu24@gmail.com>
2025-01-26 06:11:16 +0300

    komutlar eklendi

---
+++ commands.h
@@ -0,0 +1,13 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define NUM_COMMANDS 5
+
+const char *COMMANDS[NUM_COMMANDS] = {
+    "TURN_ON_LIGHT",
+    "TURN_OFF_LIGHT",
+    "INCREASE_VOLUME",
+    "DECREASE_VOLUME",
+    "PLAY_MUSIC"};
+
+#endif
